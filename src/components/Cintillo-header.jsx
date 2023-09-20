d0f5e8839ba1c587a9008fbb43885daf9c72e637
Anthony <tonyjrc2291@gmail.com>
2023-09-20 14:36:11 -0400

    Ajustes en el tamaño del header-cintillo y Footer

--- src/components/Cintillo-header.jsx
+++ src/components/Cintillo-header.jsx
@@ -4,8 +4,8 @@ import Cintillo2 from "../assets/cintillo2.svg"
 
 export default function CintilloHeader(){
     return (
-        <div className="h-32 w-full bg-[#F64613] absolute">
-            <div className="h-24 flex justify-evenly p-1 relative mt-3 bg-[#2E219E]">
+        <div className="h-20 w-full bg-[#F64613] absolute">
+            <div className="h-16 flex justify-evenly p-1 relative mt-1.5 bg-[#2E219E]">
                 <img className=" w-2/3 object-fill"  src={Cintillo} alt="Cintillo de gobierno" />
                 <img className=" w-52 object-fill"  src={Cintillo2} alt="Cintillo de gobierno" />
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
-        <footer className="h-32 w-full bg-[#2E219E] absolute bottom-0">
-            <section className="text-center mt-10 text-lg" >
+        <footer className="h-16 w-full bg-[#2E219E] absolute bottom-0">
+            <section className="text-center mt-2 text-lg" >
                 <p className="text-white">Oficina de Sistemas y Tecnologias de la Información del Instituto Nacional de la Mujer</p>
                 <span className="text-white">Derechos reservados INAMUJER 2023</span>
             </section>
