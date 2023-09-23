a2a2023c43b94df6a6ae32fee989e66e52a2c544
Anthony <tonyjrc2291@gmail.com>
2023-09-23 12:43:41 -0400

    Se agregaron los inputs del login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,74 @@
-import { TextField } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import {Button} from "@mui/material";
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-export default function Login(){
-    return(
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="min-w-fit h-96 border-8 p-8">
-                <div className="">
-                    <h2>Iniciar sesion</h2>
-                </div>
-                <form className="flex flex-col">
-                <TextField id="campo1" label="Usuario" variant="standard" />
-                <TextField id="campo2" label="Contraseña" variant="standard" />
-                    <button>Ingresar</button>
-                </form>
-            </div>
+
+export default function Login() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="grid place-items-center min-h-screen box-border">
+      <div className="min-w-fit h-96 border-2 border-gray-400 box-border absolute">
+        <div className="clip-path-mypolygon bg-[#2E219E] h-48 w-full m-0 absolute box-border pt-4">
+          <h2 className="text-center text-white text-2xl font-bold">
+            Iniciar sesión
+          </h2>
         </div>
-    )
+        <form className="flex flex-col gap-2 p-8 mt-32">
+          <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-username">
+              Username
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-username"
+              label="Username"
+              endAdornment={
+                <InputAdornment position="end">
+                  <AccountCircleIcon /> 
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <Button variant="contained" color="secondary">
+            Ingresar
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    clipPath: {
+      mypolygon: "polygon(0 0, 100% 0%, 100% 20%, 50% 41%, 0 20%)",
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require("tailwind-clip-path"),
+  ],
+};
